ba84a954bfdbd4d9c3b5b305a29b271409e034f9
TW233 <904821262@qq.com>
2025-05-13 11:28:39 +0800

    添加return

--- hello.c
+++ hello.c
@@ -3,7 +3,14 @@
 int main(int argc, char *argv[])
 {
     if (argc == 2)
+    {
         printf("Hello, %s", argv[1]);
+        return 0;
+    }
+        
     else
+    {
         printf("Missing command line!");
+        return 1;
+    }
 }
